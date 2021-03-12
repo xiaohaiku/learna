04af247f816430e1b61ef9a63fbcbf32528a818f
wangyanhai <sea.wang@nauac.com>
2021-03-12 14:42:36 +0800

    init

--- src/main/java/com/example/learnboot/LearnbootApplication.java
+++ src/main/java/com/example/learnboot/LearnbootApplication.java
@@ -2,7 +2,14 @@ package com.example.learnboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @className: LearnbootApplication
+ * @description: 
+ * @author wangYanHai
+ * @createDate 2021/3/12 14:42  
+ *
+ */
 @SpringBootApplication
 public class LearnbootApplication {
 
